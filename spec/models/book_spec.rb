840b779963598198caa7749194bd0a219be71bfa
alexyndr alexyndr <alexyndr@gmail.com>
2019-07-26 17:23:26 +0300

    add rspec test & fixes

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -1,4 +1,8 @@
 # frozen_string_literal: true
 
-RSpec.describr Book, type: :model do
+RSpec.describe Book, type: :model do
+  context 'with db colums' do
+    it { is_expected.to have_db_column(:title).of_type(:string) }
+    it { is_expected.to have_db_column(:description).of_type(:string) }
+  end
 end
