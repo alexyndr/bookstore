957da0781dda9a7e3faa61e6d67b3368e6f468fe
alexyndr alexyndr <alexyndr@gmail.com>
2019-08-01 11:21:54 +0300

    first step

--- app/models/author.rb
+++ app/models/author.rb
@@ -3,4 +3,9 @@
 # This class must doo...
 class Author < ApplicationRecord
   has_many :books
+  validates :email, presence: { strict: true }
+
+  # after_initialize do |user|
+  #   puts "Whhy strics say that user: #{user.name} is"
+  # end
 end
---
+++ spec/controllers/home_spec.rb
@@ -0,0 +1,13 @@
+RSpec.describe HomeController, type: :controller do
+  describe 'when #index' do
+    before do
+      get :index
+    end
+
+    it { is_expected.to respond_with 200 }
+  end
+
+  describe 'when routes' do
+    it { is_expected.to route(:get, '/').to(action: :index) }
+  end
+end
