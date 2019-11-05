8f6a60dd91c8646b409c46bd46ad1c955c01fd45
alexyndr belik <alexyndr@gmail.com>
2019-11-05 20:09:24 +0200

    sangrid mailer add

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,6 +62,20 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  ActionMailer::Base.smtp_settings = {
+    address:              'smtp.sendgrid.net',
+    port:                 '587',
+    domain:               'heroku.com',
+    authentication:       :plain,
+    enable_starttls_auto: true,
+    user_name:            ENV['SENDGRID_USERNAME'],
+    password:             ENV['SENDGRID_PASSWORD']
+  }
+
+  ActionMailer::Base.delivery_method = :smtp
+
+  config.action_mailer.default_url_options = { host: 'lit-spire-43064.herokuapp.com' }
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
